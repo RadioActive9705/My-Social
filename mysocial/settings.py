4c9ac10a1eef783359c8a4adab38eba68371e935
RadioActive9705 <igor.jozwiakk@gmail.com>
2025-07-04 12:17:32 +0200

    Update code

--- mysocial/settings.py
+++ mysocial/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysocial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Social_Network',
+        'NAME': 'social_network',
         'USER': 'Social_user',
         'PASSWORD': 'Uzytkownik22',
         'HOST': 'localhost',
