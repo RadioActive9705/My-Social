547f0121a04afdde3b3b2fdccc831f88da566d15
RadioActive9705 <igor.jozwiakk@gmail.com>
2025-07-04 12:05:26 +0200

    Update urls

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import views as auth_views
-from .views import post_list, register, profile_view
+from .views import post_list, register, profile_view,my_profile_redirect
+from .views import CustomLoginView
 
 urlpatterns = [
     path('',      post_list ,          name='postlist'),
     path('register/', register ,     name='register'),
-    path('login/',      auth_views.LoginView.as_view(
-                           template_name='core/login.html'
-                       ),             name='login'),
+    path('login/',     CustomLoginView.as_view(),             name='login'),
     path('logout/',     auth_views.LogoutView.as_view(
                            next_page='login'
                        ),             name='logout'),
-    path('profile/<str:username>', profile_view, name='profile')
+    path('profile/',my_profile_redirect,name='my_profile'),
+    path('profile/<str:username>/', profile_view, name='profile')
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,34 @@ from .forms import RegistationForm
 from .models import CustomUser,Profile
 from .forms import ProfileForm 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class CustomLoginView(LoginView):
+    template_name='core/login.html'
+    
+    def get_success_url(self):
+        return reverse('profile',kwargs={'username': self.request.user.username})
+
+@login_required
+def profile_view(request,username):
+    user_obj =  get_object_or_404(User,username=username)
+    
+    context={
+        'user_profile': user_obj
+    }
+    
+    
+    
+    return render(request, 'core/Profile.html', context)
+
+
+def my_profile_redirect(request):
+    current_username = request.user.username
+    
+    return redirect('profile',username=current_username)
+    
 
 def postlist(request):
     return render(request,'core/postlist.html')
@@ -26,23 +54,30 @@ def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'core/postlist.html', {'posts': posts})
 # Create your views here.
-@login_required
-def profile_view(request,username):
-    user=get_object_or_404(CustomUser,username=username)
+
+class CustomLoginView(LoginView):
+    template_name = 'core/login.html'
+
+    def get_success_url(self):
+        return reverse('profile', kwargs={'username': self.request.user.username})
+
+# @login_required
+# def profile_view(request,username):
+#     user=get_object_or_404(CustomUser,username=username)
     
-    profile=get_object_or_404(Profile,user=user)
+#     profile=get_object_or_404(Profile,user=user)
     
-    if request.user == user and request.method =="POST":
-        form = ProfileForm(request.POST, request.FILES, instance=profile)
-        if form.is_valid():
-            form.save()
-            return redirect('profile', username =username)
-        else:
-            form = ProfileForm(instance=profile)
+#     if request.user == user and request.method =="POST":
+#         form = ProfileForm(request.POST, request.FILES, instance=profile)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('profile', username =username)
+#         else:
+#             form = ProfileForm(instance=profile)
         
-        return render(request, 'core/Profile.html',{
-            'profile': profile,
-            'form': form
-        })
+#         return render(request, 'core/Profile.html',{
+#             'profile': profile,
+#             'form': form
+#         })
     
     
